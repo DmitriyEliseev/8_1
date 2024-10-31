5a1e66fef826ba8c7098a4e8a6ad88a4f62067a5
DmitriyEliseev <93958489+DmitriyEliseev@users.noreply.github.com>
2024-10-31 13:49:16 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,11 +1,10 @@
 def add_everything_up(a, b):
-    # Если a и b разных типов, возвращаем их строковое представление
-    if type(a) != type(b):
-        return str(a) + str(b)
-    else:
+    try:
         return a + b
+    except TypeError:
+        return str(a) + str(b)
 
-# Примеры использования функции
-print(add_everything_up(123.456, 'строка'))  # Результат: "123.456строка"
-print(add_everything_up('яблоко', 4215))     # Результат: "яблоко4215"
-print(add_everything_up(123.456, 7))         # Результат: 130.456
+# Пример использования
+print(add_everything_up(123.456, 'строка'))  # Вывод: '123.456строка'
+print(add_everything_up('яблоко', 4215))     # Вывод: 'яблоко4215'
+print(add_everything_up(123.456, 7))         # Вывод: 130.456
